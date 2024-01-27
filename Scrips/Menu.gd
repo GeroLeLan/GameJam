798102969cf4faca7fd841da094fb5ceb38ad3aa
Gerónimo Le Lan <gerolelant@gmail.com>
2024-01-27 13:32:26 -0300

    Créditos

--- Scrips/Menu.gd
+++ Scrips/Menu.gd
@@ -12,6 +12,8 @@ func _on_button_leaderboard_pressed():
 	get_tree().change_scene_to_file("res://Scenes/leader_board.tscn")
 
 
-
 func _on_button_settings_pressed():
 	get_tree().change_scene_to_file("res://Scenes/settings.tscn")
+	
+func _on_button_credits_pressed():
+	get_tree().change_scene_to_file("res://Scenes/credits.tscn")
