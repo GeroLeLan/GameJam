452f2524866b235b5a0fc6404fb6f2ca320ed8ff
Pablo Duran <duranpablo.alejandro@gmail.com>
2024-01-28 11:44:18 -0300

    sonido click menu/jugar

--- Scrips/Menu.gd
+++ Scrips/Menu.gd
@@ -1,11 +1,15 @@
 extends Node2D
 @onready var leaderboard_manager = preload("res://Scrips/leaderboard_manager.gd").new()
 
+
 func _ready():
 	leaderboard_manager.load_leaderboard_data() #inicializa el leaderboard si no existe
 
 func _on_button_start_pressed():
-	get_tree().change_scene_to_file("res://Scenes/game.tscn")
+	get_tree().get_nodes_in_group("sonidos")[0].get_node("click_pressed").play()
+
+	
+	
 
 
 func _on_button_leaderboard_pressed():
@@ -54,3 +58,7 @@ func _on_button_credits_mouse_entered():
 
 func _on_quit_mouse_entered():
 	get_tree().get_nodes_in_group("sonidos")[0].get_node("click").play()
+
+
+func _on_click_pressed_finished():
+	get_tree().change_scene_to_file("res://Scenes/game.tscn")
